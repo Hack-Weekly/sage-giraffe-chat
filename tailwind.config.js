fea48c602488f9943b5b2486cc4e0b2c6f57068c
vegardnx <namtvedt2@gmail.com>
2023-03-21 22:37:51 +0100

    Customized tailwind theme
    Added default styling for headings, text, buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -9,7 +10,31 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'black': '#1E222E',
+      'slate-grey': {
+        400: '#70798C',
+        600: '#444857',
+        800: '#313543'
+      },
+      'light-concrete': '#EFF2F4',
+      'pumpkin': '#D87334',
+      'burnt-pumpkin': '#9B432F',
+      'success': '#3B9C4A',
+      'error': '#C71F1F',
+      'white': '#fff',
+    },
+    fontSize: {
+      'xxs': '.625rem',
+      'xs': '.75rem',
+      'sm': '0.8rem',
+      'base': '1rem',
+      'xl': '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
   },
   plugins: [],
 };
