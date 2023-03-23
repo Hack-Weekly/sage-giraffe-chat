d01621caca52953111950ff26b19e0d0e64f17ec
francescasnatt <116296472+francescasnatt@users.noreply.github.com>
2023-03-23 17:16:40 +0000

    added block and rounded corners to login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,18 +3,18 @@
 export default function Login() {
   return (
     <div className="flex w-full h-screen justify-center items-center">
-        <form className="form p-16 bg-slate-grey-400 text-white ">
-            <h3 className='header text-center'>Login to live chat</h3>
-            <div className="form-group">
-                <label htmlFor="name">Email Address</label>
-                <input type="email" />
+        <form className="form p-16 bg-slate-grey-400 text-white rounded-lg">
+            <h3 className='header text-center mb-3'>Login to live chat</h3>
+            <div className="form-group p-2">
+                <label className="block" htmlFor="name">Email Address</label>
+                <input className="w-full" type="email" />
             </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" />
+            <div className="form-group p-2">
+                <label className="block" htmlFor="password">Password</label>
+                <input className="w-full" type="password" />
             </div>
-            <div className="form-group">
-                <button type="submit" className="primary-button  text-center">Submit</button>
+            <div className="form-group p-2">
+                <button type="submit" className="primary-button">Submit</button>
             </div>
         </form>
     </div>
